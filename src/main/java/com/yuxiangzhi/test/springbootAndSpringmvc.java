5787cee9dab08db6c2d3bcf530d526fd5879fd88
YuXZ1118 <b2286137933@163.com>
2018-04-18 11:38:29 +0800

    第一次提交

---
+++ src/main/java/com/yuxiangzhi/test/springbootAndSpringmvc.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: springbootAndSpringmvc
+ * Author:   admin
+ * Date:     2018/4/17 15:47
+ * Description: demo
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.yuxiangzhi.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈demo〉
+ *
+ * @author admin
+ * @create 2018/4/17
+ * @since 1.0.0
+ */
+@RestController
+public class springbootAndSpringmvc {
+    @Autowired
+    //  获取配置文件中的值
+    Environment environment;
+    @RequestMapping("/demo")
+    public String get(){
+        return "helloSpringboot"+"\t"+environment.getProperty("url");
+    }
+}
---
+++ src/main/java/com/yuxiangzhi/test/test.java
@@ -0,0 +1,26 @@
+
+package com.yuxiangzhi.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈class〉
+ *
+ * @author admin
+ * @create 2018/4/17
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class test {
+
+    /**
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(test.class,args);
+    }
+}
